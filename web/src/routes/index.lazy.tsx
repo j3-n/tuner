16063a32960227d51d7d7cfb10db5acd4a057936
Jacob Padley <jacob5180@hotmail.co.uk>
2024-02-17 16:22:35 +0000

    Add logo and title to front page

--- web/src/routes/index.lazy.tsx
+++ web/src/routes/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { Blink } from '../components/blink';
-import Cookies from 'js-cookie';
+import logoImg from "../assets/logo.png"
 
 export const Route = createLazyFileRoute('/')({
   component: Page,
@@ -9,10 +9,12 @@ export const Route = createLazyFileRoute('/')({
 function Page() {
   return (
     <div className="p-2 fixed justify-center items-center w-full">
-      <h1>TUNER</h1>
 
-      <p>{Cookies.get("TUNER_SESSION")}</p>
-      <div className="w-full text-slate-100">
+      <img src={logoImg} className="h-48 rounded-3xl inline ml-10"></img>
+
+      <h1 className="text-8xl font-bold inline ml-10">TUNER</h1>
+
+      <div className="w-full text-slate-100 px-10 mt-10">
         <div className="align-middle gap-x-10 w-full h-96 grid grid-cols-2 mb-20">
           <div className="bg-sky-600 rounded-xl"><Blink to='/create'><p className="text-6xl">Create Game</p></Blink></div>
           <div className="bg-fuchsia-700 rounded-xl"><Blink to='/lobby'><p className="text-6xl">Join Game</p></Blink></div>
