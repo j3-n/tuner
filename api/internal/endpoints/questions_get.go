200b1982f2bf3ddd2042215ac85a98ce74041af4
Tworye01 <natanwojcik15@gmail.com>
2024-02-17 16:05:12 +0000

    fix:models

--- api/internal/endpoints/questions_get.go
+++ api/internal/endpoints/questions_get.go
@@ -2,10 +2,10 @@ package endpoints
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/j3-n/tuner/api/internal/quiz"
+	"github.com/j3-n/tuner/api/internal/models"
 )
 
 func GetQuestions(c *fiber.Ctx) error {
-	eer := quiz.QuestionsSet[0]
+	eer := models.QuestionsSet[0]
 	return c.JSON(eer)
 }
