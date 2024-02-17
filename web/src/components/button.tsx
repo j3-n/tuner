91168f96017b85e2df282846c72ddba1853b6265
Joseph Beck <joebeck03@icloud.com>
2024-02-17 17:27:49 +0000

    feat: start types for game

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -5,7 +5,10 @@ type Props = {
   children?: ReactNode;
 }
 
-export const ButtonComponent: React.FC<Props> = ({ onClick, children }) => {
+export const ButtonComponent: React.FC<Props> = ({
+  onClick,
+  children
+}): JSX.Element => {
   return (
     <div className="rounded-lg p-2 border-2 border-teal-200 bg-teal-100 hover:bg-teal-200">
       <button onClick={onClick}>
