e19931acad3da9cffb9d90e430f27043a4dcd521
Tworye01 <natanwojcik15@gmail.com>
2024-02-17 21:16:03 +0000

    Questions to all

--- api/internal/game/guessing.go
+++ api/internal/game/guessing.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"encoding/json"
+
 	"github.com/j3-n/tuner/api/internal/models"
 )
 
@@ -9,7 +11,12 @@ func StartRound(l *models.Lobby, playerID int, guess string) {
 	shit := []byte("START")
 	l.BroadcastToAllPlayers(shit)
 
-	
+	message, err := json.Marshal(models.QuestionsSet)
+	if err != nil {
+		return
+	}
+	l.BroadcastToAllPlayers(message)
+
 	// Send questions to frontend with guesses
 	// Wait
 	// Points -> Leaderboards
