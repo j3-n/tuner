de126121d1c6fd7f535b06c29f67aedf2cef215b
Tworye01 <natanwojcik15@gmail.com>
2024-02-17 21:12:12 +0000

    Broadcasting

--- api/internal/game/guessing.go
+++ api/internal/game/guessing.go
@@ -1,7 +1,15 @@
 package game
 
-func StartRound(lobbyID int, playerID int, guess string) {
+import (
+	"github.com/j3-n/tuner/api/internal/models"
+)
+
+func StartRound(l *models.Lobby, playerID int, guess string) {
 	// Broadcast that game has started
+	shit := []byte("START")
+	l.BroadcastToAllPlayers(shit)
+
+	
 	// Send questions to frontend with guesses
 	// Wait
 	// Points -> Leaderboards
