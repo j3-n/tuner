e67fd6836a54daeebea36e932a798148096fad2b
Joseph Beck <joebeck03@icloud.com>
2024-02-18 11:43:35 +0000

    adjust game length

--- api/internal/game/start.go
+++ api/internal/game/start.go
@@ -9,7 +9,7 @@ import (
 func StartRound(l *models.Lobby) {
 	// Generate quiz
 	if l.State == models.Waiting {
-		l.Questions = l.GenerateQuiz(5)
+		l.Questions = l.GenerateQuiz(3)
 		l.Points = map[string]int{}
 		l.State = models.Guessing
 		NewRound(l)
