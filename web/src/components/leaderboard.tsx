53952db2b2becee83c1da901791b01d12886dadc
Jacob Padley <jacob5180@hotmail.co.uk>
2024-02-18 11:31:04 +0000

    buggle wuggle

--- web/src/components/leaderboard.tsx
+++ web/src/components/leaderboard.tsx
@@ -6,10 +6,10 @@ type Props = {
 };
 
 function compare( a: PlayerScore, b: PlayerScore ) {
-  if ( a.score < b.score ){
+  if ( a.score > b.score ){
     return -1;
   }
-  if ( a.score > b.score ){
+  if ( a.score < b.score ){
     return 1;
   }
   return 0;
@@ -25,7 +25,7 @@ export const LeaderboardComponent: React.FC<Props> = ({ leaderboard }): JSX.Elem
     <>
       <div className="grid grid-cols-7 bg-slate-800 bg-opacity-75 rounded-xl p-5 mt-10 mx-10">
       {leaderboard && getSorted(leaderboard.scores).map((score: PlayerScore, index: number) =>
-        <PlayerLeaderboardComponent key={index} place={index=1} name={score.playerName} url={score.iconURL} score={score.score}></PlayerLeaderboardComponent>
+        <PlayerLeaderboardComponent key={index} place={index+1} name={score.playerName} url={score.iconURL} score={score.score}></PlayerLeaderboardComponent>
       )}
       </div>
     </>
